a940e73dd051419b4d95bbed2b867df0a59916f1
casperjwk <casperjwk@gmail.com>
2026-06-23 16:54:23 +0900

    feat: implement ProductCard component and update App to display product list

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,33 @@
-import Card from './components/Card'
+import Card from './components/Card';
+import ProductCard from './components/ProductCard';
 const product = { name: "무선 키보드", price: 39000, brand: "EZTECH"};
 const {name, price, brand} = product;
 
   console.log(name,price,brand);
 
-function App() {
+  
+  
+  function App() {
+    
+    
+    const products = [
+        { id: 1, name: "무선 키보드", price: 39000, brand: "EZTECH" },
+        { id: 2, name: "무선 마우스", price: 29000, brand: "EZTECH" },
+        { id: 3, name: "모니터", price: 199000, brand: "EZTECH" }
+      ];
+      
 
-
-  return (
+    
+    const listbyMap = products.map(product => <ProductCard key={product.id} name={product.name} price={product.price} brand={product.brand}/>);
+    
+    return (
+      
+    <>
     <Card name={name} price={price} brand={brand} />
-  )
+
+  {listbyMap}
+  </>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,11 @@
+import styles from "./ProductCard.module.css"
+
+export default function ProductCard({ name, price , brand }) {
+  return (
+   <article className={styles.cardItem}>
+      <h3>{name}</h3>
+      <p>{price}</p>
+      <p>{brand}</p>
+   </article>
+  );
+}
